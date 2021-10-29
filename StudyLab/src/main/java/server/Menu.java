afb7f8cd95a20c3eb7be5fa1be138abad66e9a4f
Sshack22 <ss223023@my.stchas.edu>
2021-10-29 18:30:09 -0500

    test

--- StudyLab/src/main/java/server/Menu.java
+++ StudyLab/src/main/java/server/Menu.java
@@ -50,8 +50,8 @@ public class Menu extends HttpServlet {
 			response.sendRedirect("memberRegister.jsp");
 		}
 		if (request.getParameter("button3") != null) {
-			//test git 
-			
+			//test git  
+			 
 			
 		}
 		
